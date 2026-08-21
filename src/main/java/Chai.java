ba87306af7c7242edd1436fc57952007a063b990
joash-chen <joashchen@outlook.com>
2026-08-21 15:29:45 +0800

    Renamed chatbot to Chai

--- src/main/java/Chai.java
+++ src/main/java/Chai.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Chai {
     public static void main(String[] args) {
         String banner = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
