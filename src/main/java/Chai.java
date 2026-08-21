fb68b66bee6bba65f2c19ee6dc6f2e89cc056053
joash-chen <joashchen@outlook.com>
2026-08-21 17:42:03 +0800

    Added support for mark

--- src/main/java/Chai.java
+++ src/main/java/Chai.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.util.function.IntFunction;
 
 public class Chai {
     public static void main(String[] args) {
         String banner = " ▄████▄   ██░ ██  ▄▄▄      ██▓\n" +
-                        "▒██▀ ▀█  ▓██░ ██▒▒████▄        \n" +
+                        "▒██▀ ▀█  ▓██░ ██▒▒████▄    ▒░░ \n" +
                         "▒▓█    ▄ ▒██▀▀██░▒██  ▀█▄  ▒██░\n" +
                         "▒▓▓▄ ▄██▒░▓█ ░██ ░██▄▄▄▄██ ▒██░\n" +
                         "▒ ▓███▀ ░░▓█▒░██▓ ▓█   ▓██▒░██░\n" +
@@ -14,7 +15,9 @@ public class Chai {
 
         Scanner scanner = new Scanner(System.in);
         String[] tasks = new String[100];
+        boolean[] tasksCompleted = new boolean[100];
         int taskCount = 0;
+        IntFunction<String> taskDescription = index -> "[" + (tasksCompleted[index] ? "X" : " ") + "] " + tasks[index];
 
         System.out.println(introduction);
         while (true) {
@@ -26,8 +29,31 @@ public class Chai {
                 break;
             } else if (command.equals("list")) {
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + ". " + taskDescription.apply(i));
                 }
+            } else if (command.equals("mark") || command.startsWith("mark ")) {
+                String[] parts = command.split("\\s+");
+                String output;
+
+                if (parts.length != 2) {
+                    output = "Please use the format: mark <task number>";
+                } else {
+                    try {
+                        int taskNumber = Integer.parseInt(parts[1]);
+
+                        if (taskNumber < 1 || taskNumber > taskCount) {
+                            output = "That task number does not exist. Please choose a number from 1 to "
+                                    + taskCount + ".";
+                        } else { // command && task are valid
+                            tasksCompleted[taskNumber - 1] = true;
+                            output = "Marked task " + taskNumber + " as done:\n  " +
+                                    taskDescription.apply(taskNumber - 1);
+                        }
+                    } catch (NumberFormatException e) {
+                        output = "The task number must be a whole number.";
+                    }
+                }
+                System.out.println(output);
             } else {
                 if (taskCount < tasks.length) {
                     tasks[taskCount++] = command;
