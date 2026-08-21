01aa8e84f6dea9b86e831fdd0da83c88f26a6a2e
joash-chen <joashchen@outlook.com>
2026-08-21 16:45:37 +0800

    refactor: improved code cleaniless and structure

--- src/main/java/Chai.java
+++ src/main/java/Chai.java
@@ -2,35 +2,32 @@ import java.util.Scanner;
 
 public class Chai {
     public static void main(String[] args) {
+        String banner = " ▄████▄   ██░ ██  ▄▄▄      ██▓\n" +
+                        "▒██▀ ▀█  ▓██░ ██▒▒████▄        \n" +
+                        "▒▓█    ▄ ▒██▀▀██░▒██  ▀█▄  ▒██░\n" +
+                        "▒▓▓▄ ▄██▒░▓█ ░██ ░██▄▄▄▄██ ▒██░\n" +
+                        "▒ ▓███▀ ░░▓█▒░██▓ ▓█   ▓██▒░██░\n" +
+                        "░ ░▒ ▒  ░ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒░\n";
+        String separator = "____________________________________________________________";
+        String introduction = separator + '\n' + banner + '\n' + "Hey I'm Chai :)\n" + "What do you need?\n" + separator;
+
         Scanner scanner = new Scanner(System.in);
 
-        String banner = " ▄████▄   ██░ ██  ▄▄▄      ██▓\n" +
-                "▒██▀ ▀█  ▓██░ ██▒▒████▄        \n" +
-                "▒▓█    ▄ ▒██▀▀██░▒██  ▀█▄  ▒██░\n" +
-                "▒▓▓▄ ▄██▒░▓█ ░██ ░██▄▄▄▄██ ▒██░\n" +
-                "▒ ▓███▀ ░░▓█▒░██▓ ▓█   ▓██▒░██░\n" +
-                "░ ░▒ ▒  ░ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒░";
-
-        System.out.println("____________________________________________________________\n");
-        System.out.println(banner + '\n');
-        System.out.println("Hello! I'm Chai.");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-
+        System.out.println(introduction);
         while (true) {
             String command = scanner.nextLine();
 
-            System.out.println("____________________________________________________________");
+            System.out.println(separator);
 
             if (command.equals("bye")) {
                 break;
             }
 
             System.out.println(command);
-            System.out.println("____________________________________________________________");
+            System.out.println(separator);
         }
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("See you soon!");
+        System.out.println(separator);
     }
 }
