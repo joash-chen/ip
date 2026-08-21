dc8665a6ed5b0b34f6d91e507b56d5d8695d1e3e
joash-chen <joashchen@outlook.com>
2026-08-21 16:16:52 +0800

    Added greeting

--- src/main/java/Chai.java
+++ src/main/java/Chai.java
@@ -1,10 +1,17 @@
 public class Chai {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        String banner = " ▄████▄   ██░ ██  ▄▄▄      ██▓\n" +
+                "▒██▀ ▀█  ▓██░ ██▒▒████▄        \n" +
+                "▒▓█    ▄ ▒██▀▀██░▒██  ▀█▄  ▒██░\n" +
+                "▒▓▓▄ ▄██▒░▓█ ░██ ░██▄▄▄▄██ ▒██░\n" +
+                "▒ ▓███▀ ░░▓█▒░██▓ ▓█   ▓██▒░██░\n" +
+                "░ ░▒ ▒  ░ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒░";
+        System.out.println("____________________________________________________________\n");
+        System.out.println(banner + '\n');
+        System.out.println("Hello! I'm Chai.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
