e020fb681141642553b2cf88ded1b79878bff6f5
joash-chen <joashchen@outlook.com>
2026-08-24 11:06:21 +0800

    Added Todo task class

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+/** A task without a date or time attached to it. */
+public class Todo extends Task {
 
-public class Todo {
+    /** Creates an incomplete todo with the given description. */
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
 }
